1a62d7c1840c29a34f79e844d618a8ec64f26e41
Clark Kampfe <clark@bellycard.com>
2015-05-26 23:41:15 -0500

    initial go at yielding list of policies

--- lib/xapigee.ex
+++ lib/xapigee.ex
@@ -2,17 +2,23 @@ defmodule Xapigee do
   def start(["from", filename]) do
     yaml_doc = Xapigee.Loader.load_yaml(filename)
 
-    tags =
+    # build default.xml and get extract policies
+    [{proxy_endpoint, policies}] =
       yaml_doc
-      |> Enum.map(&Xapigee.Composer.build(&1, []))
+      |> Enum.map(&Xapigee.Composer.build(&1, {[], []}))
       |> Enum.map(
-           fn doc ->
-             ["<ProxyEndpoint name=\"default\">\n"]
-             ++ doc
-             ++ ["</ProxyEndpoint>"]
+           fn {doc, policies} ->
+             {
+               ["<ProxyEndpoint name=\"default\">\n"]
+               ++ doc
+               ++ ["</ProxyEndpoint>"],
+               policies
+             }
            end
          )
 
-    IO.puts tags
+
+    IO.inspect policies
+    IO.puts proxy_endpoint
   end
 end
--- lib/xapigee/composer.ex
+++ lib/xapigee/composer.ex
@@ -1,39 +1,52 @@
 defmodule Xapigee.Composer do
-  def build([], doc), do: Enum.reverse doc
+  def build([], {doc, policies}), do: {Enum.reverse(doc), policies}
 
-  def build([{'preflow', props}|rest], doc) do
+  def build([{'preflow', props}|rest], {doc, policies}) do
+
+    {preflows, new_policies} = build(props, {[], []})
+
     preflow = """
     <Preflow>
-      <%= res %>
+      <%= preflows %>
     </Preflow>
-    """ |> EEx.eval_string [res: build(props, [])]
+    """ |> EEx.eval_string [preflows: preflows]
 
-    build(rest, [preflow|doc])
+    build(rest, {[preflow|doc], new_policies ++ policies})
   end
 
-  def build([{'flows', flows}|rest], doc) do
-    flow_props =
+  def build([{'flows', flows}|rest], {doc, policies}) do
+    {flow_props, new_policies} =
       flows
-      |> Enum.flat_map(
+      |> Enum.map(
         fn({flow_name, flow_desc}) ->
-          [
-          """
-          <Flow name=<%= flow_name %>>
-            <%= flow_desc_res %>
-          </Flow>
-          """ |> EEx.eval_string [
-            flow_name: flow_name,
-            flow_desc_res: build(flow_desc, [])]
-          ]
+
+          {flow_desc_res, np} = build(flow_desc, {[], []})
+
+          {
+            ("""
+            <Flow name=<%= flow_name %>>
+              <%= flow_desc_res %>
+            </Flow>
+            """ |> EEx.eval_string [
+              flow_name: flow_name,
+              flow_desc_res: flow_desc_res
+            ]), np
+          }
+        end
+      )
+      |> Enum.reduce(
+        {[], []},
+        fn({flow, policy}, {flow_acc, policy_acc}) ->
+          {[flow|flow_acc], policy ++ policy_acc}
         end
       )
 
-    flows = ["<Flows>"] ++ flow_props ++ "</Flows>\n"
+    flows = ["<Flows>"] ++ flow_props ++ ["</Flows>\n"]
 
-    build(rest, [flows|doc])
+    build(rest, {flows ++ doc, new_policies ++ policies})
   end
 
-  def build([{'request', props}|rest], doc) do
+  def build([{'request', props}|rest], {doc, policies}) do
     request = """
       <Request>
       <%= for item <- props do %>
@@ -42,14 +55,14 @@ defmodule Xapigee.Composer do
       </Request>
     """ |> EEx.eval_string [props: props]
 
-    build(rest, [request|doc])
+    build(rest, {[request|doc], Enum.map(props, &to_string(&1)) ++ policies})
   end
 
-  def build([{'response', :null}|rest], doc) do
-    build(rest, ["  <Response />"|doc])
+  def build([{'response', :null}|rest], {doc, policies}) do
+    build(rest, {["  <Response />"|doc], policies})
   end
 
-  def build([{'response', props}|rest], doc) do
+  def build([{'response', props}|rest], {doc, policies}) do
     response = """
     <Response>
     <%= for item <- props do %>
@@ -58,42 +71,47 @@ defmodule Xapigee.Composer do
     </Response>
     """ |> EEx.eval_string [props: props]
 
-    build(rest, [response|doc])
+    build(rest, {[response|doc], policies})
   end
 
-  def build([{'path', path}, {'verb', verb}|rest], doc) do
+  def build([{'path', path}, {'verb', verb}|rest], {doc, policies}) do
     match_condition = """
     <Condition>(proxy.pathsuffix MatchesPath "<%= path %>") and (request.verb = "<%= verb %>")</Condition>
     """
     |> EEx.eval_string [path: path, verb: verb]
 
-    build(rest, [match_condition|doc])
+    build(rest, {[match_condition|doc], policies})
   end
 
-  def build([{'httpproxyconnection', props}|rest], doc) do
+  def build([{'httpproxyconnection', props}|rest], {doc, policies}) do
+    {httpproxy_props, pol} = build(props, {[], []})
+
     httpproxyconnection = """
     <HTTPProxyConnection>
     <%= props %>
     </HTTPProxyConnection>
-    """ |> EEx.eval_string [props: build(props, [])]
+    """ |> EEx.eval_string [props: httpproxy_props]
 
-    build(rest, [httpproxyconnection|doc])
+    build(rest, {[httpproxyconnection|doc], pol ++ policies})
   end
 
-  def build([{'basepath', basepath}|rest], doc) do
+  def build([{'basepath', basepath}|rest], {doc, policies}) do
     base_path = """
       <BasePath><%= base_path %></BasePath>
       <VirtualHost>secure</VirtualHost>
     """ |> EEx.eval_string [base_path: basepath]
 
-    build(rest, [base_path|doc])
+    build(rest, {[base_path|doc], policies})
   end
 
-  def build([{'routerules', routerules}|rest], doc) do
+  def build([{'routerules', routerules}|rest], {doc, policies}) do
     route_rule_props =
       routerules
       |> Enum.flat_map(
          fn({route_name, route_desc}) ->
+
+           {route_desc_res, pol} = build(route_desc, {[], []})
+
            [
            """
            <RouteRule name=<%= route_name %>>
@@ -101,15 +119,20 @@ defmodule Xapigee.Composer do
            </RouteRule>
            """ |> EEx.eval_string [
              route_name: route_name,
-             route_desc_res: build(route_desc, [])]
+             route_desc_res: route_desc_res]
            ]
          end
       )
 
-    build(rest, [route_rule_props|doc])
+    build(rest, {route_rule_props ++ doc, policies})
   end
 
-  def build([{'faultrules', faultrules}|rest], doc) do
+  def build([{'faultrules', faultrules}|rest], {doc, policies}) do
+    new_policies =
+      faultrules
+      |> Enum.map(fn({_, [name, [{_,_}]]}) -> to_string(name) end)
+
+
     fault_rule_props =
       faultrules
       |> Enum.flat_map(
@@ -134,6 +157,6 @@ defmodule Xapigee.Composer do
          end
       )
 
-    build(rest, [fault_rule_props|doc])
+    build(rest, {fault_rule_props ++ doc, new_policies ++ policies})
   end
 end
